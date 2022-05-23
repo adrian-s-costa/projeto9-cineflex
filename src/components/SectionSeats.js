106215e09312090da85caff1bcbe59d2437627e2
pink_glb <adriancosta1215@gmail.com>
2022-05-23 10:20:15 -0400

    Selecao de assentos com logica quase terminada

--- src/components/SectionSeats.js
+++ src/components/SectionSeats.js
@@ -2,12 +2,39 @@ import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
+
+
 export default function SectionSeats(){
     
     const [movie, setMovie] = useState([])
     const [seats, setSeats] = useState([])
-    const { idSessao } = useParams();
+    const { idSessao } = useParams()
+    const [name, setName] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [seatsSelec, setSeatsSelec] = useState([])
 
+    function submitForm(event){
+        event.preventDefault()
+        const data = {
+            name: name,
+            cpf: cpf
+        }
+        console.log(data)
+    }
+
+    function Selecionar(seat){
+        console.log(seat)
+        if(seat.selected === false){
+            console.log('selecionou')
+            console.log(seatsData)
+            return seat.selected = true
+        }else{
+            console.log('deselecionou')
+            console.log(seatsData)
+            return seat.selected = false
+        }
+    }
+    
     useEffect(()=>{
         const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idSessao}/seats`)
 
@@ -15,8 +42,19 @@ export default function SectionSeats(){
             setSeats(promise.data.seats)
             setMovie(promise.data)
         })
+
+        
     }, [])
-    console.log(movie.movie)
+    
+    let seatsData = seats.map(value => {
+        return{
+            ...value,
+            selected: false,
+        }
+    })
+
+    console.log(seatsData)
+
     return(
         <>
         <div className='sectionSeats'>
@@ -24,9 +62,15 @@ export default function SectionSeats(){
                 <h2>Selecione o(s) assentos</h2>
             </div>
             <div className='seats'>
-                {seats.map((seat)=> (
-                    <div className='seat'>{seat.name}</div>
-                ))}
+                {seatsData.map(seat=> {
+                    if(seat.isAvailable === false){
+                        return <div className='seat y'> {seat.name} </div>
+                    }
+                    else{
+                        return <div className={`seat ${seat.selected ? 'g' : 'gray'}`} onClick={() => Selecionar(seat)}>{seat.name}</div>
+                    }
+                    
+                })}
             </div>
             
             <div className='sub1'>
@@ -47,14 +91,17 @@ export default function SectionSeats(){
                 
         </div>
         <div className='inputs'>
-            <h6>Nome do comprador</h6>
-            <input className='input'></input>
-            <h6>CPF do comprador</h6>
-            <input className='input'></input>
+            <form onSubmit={submitForm}>
+                <h6>Nome do comprador</h6>
+                <input className='input' required type='text' onChange={(e) => setName(e.target.value)} value={name}/>
+                <h6>CPF do comprador</h6>
+                <input className='input' required onChange={(e) => setCpf(e.target.value)} value={cpf}/>
+                <div className='btnSeats'>
+                    <button className='btnSeatsW' type='submit'>Reservar assentos</button>
+                </div>
+            </form>
         </div>
-        <div className='btnSeats'>
-                <button className='btnSeatsW'>Reservar assentos</button>
-        </div>
+        
         <footer className='footer'>
         {movie.length === 0 ? <div class="lds-hourglass"></div>:
             <>
